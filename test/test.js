80d005a909b548b5aed4911cc5141729a68d1e28
Johnny Hu <jgeehen.hu@gmail.com>
2017-06-23 22:11:02 -0700

    Create a basic module with a function that generates a name from a list

---
+++ test/test.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// require chai — assertion library
+const expect = require('chai').expect;
+
+// require your module
+const utils = require('../lib/index');
+
+// use mocha
+// describe your module
+describe('#createName', function() {
+  describe('with no arguments', function() {
+    // what should the module do?
+    it('should return a random name', function() {
+      // run the module functionality
+      var result = utils.createName();
+
+      // test the result to the expected outcome
+      expect(result).to.equal('hi');
+    });
+  });
+});
